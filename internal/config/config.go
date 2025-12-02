a19f37f300b4d7ec0b5192ad1bea139b2aac82a4
RAHUL KUMAR <stkwhy000@gmail.com>
2025-12-02 23:55:31 +0530

    learn basic flow with postgress sql

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,21 @@
 package config
+
+import "os"
+
+type Config struct {
+	DBHost string
+	DBUser string
+	DBPass string
+	DBName string
+	DBPort string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		DBHost: os.Getenv("DB_Host"),
+		DBUser: os.Getenv("DB_User"),
+		DBPass: os.Getenv("DB_Pass"),
+		DBName: os.Getenv("DB_Name"),
+		DBPort: os.Getenv("DB_Port"),
+	}
+}
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,36 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/RahulKumar9988/go-basic-backend/internal/config"
+)
+
+var DB *sql.DB
+
+func Connext(cfg *config.Config) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		cfg.DBHost,
+		cfg.DBPass,
+		cfg.DBUser,
+		cfg.DBPort,
+		cfg.DBName,
+	)
+
+	var error error
+
+	DB, error = sql.Open("mySql", dsn)
+
+	if error != nil {
+		log.Fatal("error connecting db connection:", error)
+	}
+
+	if error = DB.Ping(); error != nil {
+		log.Fatal("failed to connect to mySql:", error)
+	}
+
+	log.Println("mySql connected sucessfully")
+}
