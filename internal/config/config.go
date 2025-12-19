f597417a7f6aa72d52403a64d7caab6f406173f5
RAHUL KUMAR <stkwhy000@gmail.com>
2025-12-19 07:40:08 +0530

    fix conflict

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,36 +1,23 @@
 package config
 
 import (
-<<<<<<< HEAD
-	"log"
-=======
-	"fmt"
 	// "log"
->>>>>>> 78f72f6493074c6e33cd9e2e109d32cec8e4be68
+	"log"
 	"os"
 )
 
 type Config struct {
-<<<<<<< HEAD
 	DBHost string `yaml:"DB_HOST" env:"DB_HOST" env-required:"true"`
 	DBUser string `yaml:"DB_HOST" env:"DB_HOST" env-required:"true"`
 	DBPass string `yaml:"DB_PASSWORD" env:"DB_PASSWORD" env-required:"true"`
 	DBName string `yaml:"DB_NAME" env:"DB_NAME" env-required:"true"`
 	DBPort string `yaml:"DB_PORT" env:"DB_PORT" env-required:"true"`
-=======
-	DBHost string
-	DBUser string
-	DBName string
-	DBPass string
-	DBPort string
->>>>>>> 78f72f6493074c6e33cd9e2e109d32cec8e4be68
 }
 
 func LoadConfig() *Config {
 	cfg := &Config{
 		DBHost: os.Getenv("DB_HOST"),
 		DBUser: os.Getenv("DB_USER"),
-<<<<<<< HEAD
 		DBPass: os.Getenv("DB_PASSWORD"),
 		DBName: os.Getenv("DB_Name"),
 		DBPort: os.Getenv("DB_Port"),
@@ -41,18 +28,4 @@ func LoadConfig() *Config {
 	}
 
 	return cfg
-
-=======
-		DBName: os.Getenv("DB_NAME"),
-		DBPass: os.Getenv("DB_PASS"),
-		DBPort: os.Getenv("DB_PORT"),
-	}
-
-	if cfg.DBHost == "" || cfg.DBName == "" || cfg.DBUser == "" || cfg.DBPass == "" || cfg.DBPort == "" {
-		// log.Fatal("DB CONNECTGION FAILED: MISSING ENV")
-		fmt.Println("DB CONNECTGION FAILED: MISSING ENV")
-	}
-
-	return cfg
->>>>>>> 78f72f6493074c6e33cd9e2e109d32cec8e4be68
 }
